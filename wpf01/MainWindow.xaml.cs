c3c76702e958b034f23fbcfeb4bd9054076d7027
pablo <jpablito.sei@gmail.com>
2025-02-14 10:07:39 -0300

    Agregar archivos de proyecto.

---
+++ wpf01/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+           
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Juego> juegos = new List<Juego>(); 
+            juegos.Add(new Juego { Puntaje1 = 2, Puntaje2 = 3, Eq1 = "Barcelona", Eq2 = "Real madril", Progreso = 80 });
+            juegos.Add(new Juego { Puntaje1 = 2, Puntaje2 = 3, Eq1 = "Barcelona", Eq2 = "Real madril", Progreso = 90 });
+            juegos.Add(new Juego { Puntaje1 = 2, Puntaje2 = 3, Eq1 = "Barcelona", Eq2 = "Real madril", Progreso = 80 });
+            juegos.Add(new Juego { Puntaje1 = 2, Puntaje2 = 3, Eq1 = "Barcelona", Eq2 = "Real madril", Progreso = 80 });
+            juegos.Add(new Juego { Puntaje1 = 2, Puntaje2 = 3, Eq1 = "Barcelona", Eq2 = "Real madril", Progreso = 10 });
+            lbJuego.ItemsSource = juegos;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(lbJuego.SelectedItem != null)
+            {
+                MessageBox.Show("Juego Seleccionado" + (lbJuego.SelectedItems as Juego).Eq1 + " " + (lbJuego.SelectedItems as Juego).Eq2);
+            }
+        }
+    }
+
+    public class Juego
+    {
+        public int Puntaje1 { get; set; }
+        public int Puntaje2 { get; set; }
+        public string Eq1 { get; set; }
+        public string Eq2 { get; set; }
+        public int Progreso { get; set; }   
+
+
+    }
+}
---
+++ wpf01/Suma.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf01
+{
+    public class Suma : INotifyPropertyChanged
+    {
+        private string num1;
+        private string num2;
+        private string resultado;
+
+        public string Num1 
+        {
+            get
+            { 
+                return num1;
+            }
+            set
+            {
+                int numero;
+                bool res = int.TryParse(value, out numero);
+                if (res) num1=value;
+                OnPropertyChanged("Num1");
+                OnPropertyChanged("Resultado");
+
+            }
+        }
+        public string Num2
+        {
+            get
+            {
+                return num2;
+            }
+            set
+            {
+                int numero;
+                bool res = int.TryParse(value, out numero);
+                if (res) num2 = value;
+                OnPropertyChanged("Num2");
+                OnPropertyChanged("Resultado");
+
+            }
+        }
+        public string Resultado
+        {
+            get
+            {
+                int res= int.Parse(num1) + int.Parse(num2);
+                return res.ToString();
+            }
+            set
+            {
+                int res = int.Parse(num1) + int.Parse(num2);
+                resultado = res.ToString();
+                OnPropertyChanged("Resultado");
+
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
